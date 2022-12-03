dc1a7786af6c26cd8956728f53986abee0b52890
Thomas Harrison <twh2898@vt.edu>
2022-12-02 21:08:13 -0500

    Removed virtual from BufferArray destructor

--- include/glpp/Buffer.hpp
+++ include/glpp/Buffer.hpp
@@ -150,7 +150,7 @@ namespace glpp {
         BufferArray(const BufferArray &) = delete;
         BufferArray & operator=(const BufferArray &) = delete;
 
-        virtual ~BufferArray();
+        ~BufferArray();
 
         GLuint getArrayId() const;
 
