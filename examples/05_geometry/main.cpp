a05678add81491b48db55c358e4fc93d9ff8e50c
Thomas Harrison <twh2898@vt.edu>
2022-12-18 22:25:47 -0500

    Remove triangle shader source

--- examples/05_geometry/main.cpp
+++ examples/05_geometry/main.cpp
@@ -18,24 +18,6 @@ using namespace glpp::extra;
 using namespace std;
 using namespace glm;
 
-static const char * triangleVertexShaderSource = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aCol;
-out vec3 color;
-void main() {
-    gl_Position = vec4(aPos, 1.0);
-    color = aCol;
-})";
-
-static const char * triangleFragmentShaderSource = R"(
-#version 330 core
-in vec3 color;
-out vec4 FragColor;
-void main() {
-    FragColor = vec4(color, 1.0);
-})";
-
 static const char * vertexShaderSource = R"(
 #version 330 core
 layout (location = 0) in vec3 aPos;
