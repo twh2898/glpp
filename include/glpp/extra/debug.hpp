f6098cf2c610498f5af5fdf57c8cc271ec5f1cd1
Thomas Harrison <twh2898@vt.edu>
2023-07-09 14:48:55 -0400

    Add docstring to debug.hpp

--- include/glpp/extra/debug.hpp
+++ include/glpp/extra/debug.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
 namespace glpp::extra {
+    /**
+     * Enable GL_DEBUG_OUTPUT and setup glDebugMessageCallback.
+     *
+     * This function will write a message to stderr noting that debug messages
+     * are enabled.
+     */
     void initDebug();
 }
